62f03bab35c47201976ad4a0d933afcb2d1b7c5f
josiasMO <jotamo.jmo10@gmail.com>
2018-03-21 20:52:32 -0300

    Added support to IndexedDB

--- main.js
+++ main.js
@@ -1,22 +1,11 @@
 const {app, BrowserWindow} = require('electron');
 const path = require('path');
 const url = require('url');
-const mqtt = require('mqtt');
-
-// var indexedDB;
-
-require('electron-reload')(__dirname, {
-  electron: path.join(__dirname, 'node_modules', '.bin', 'electron', 'src'),
-  hardResetMethod: 'exit'
-});
 
 let win;
-let client;
 
 function createWindow () {
   win = new BrowserWindow({width: 800, height: 600, fullScreen: true});
-
-
   // load the dist folder from Angular
   win.loadURL(url.format({
     pathname: path.join(__dirname, 'dist/index.html'),
@@ -25,53 +14,14 @@ function createWindow () {
   }));
 
   // Open the DevTools optionally:
-  // win.webContents.openDevTools()
+  win.webContents.openDevTools()
 
   win.on('closed', () => {
     win = null
-    client.end();
   });
-}
-
-
-
-function connectMQTT() {
-
-
-
-  var options = {
-    port: 1883,
-    username: "ufsm_lora",
-    password: "ttn-account-v2.ASUiRfuLwBCxz31WhJM0xCkjaMwOKFWQL47C2Xg0H78",
-    keepalive: 60
-  };
-
-  client  = mqtt.connect("mqtt://brazil.thethings.network", options);
-
-  client.on('connect', function () {
-    console.log("Connected");
-    client.subscribe('ufsm_lora/devices/rhf76-052/up');
-  });
-
-  client.on('message', function (topic, message) {
-    // message is Buffer
-    var msg= JSON.parse(message.toString());
-    var string_msg = Buffer.from(msg.payload_raw, 'base64').toString()
-    console.log(string_msg);
-    storeDB(string_msg);
-
-  });
-
-}
-
-function storeDB(value) {
-
-
 }
 
 app.on('ready', createWindow);
-
-app.on('ready', connectMQTT);
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
---
+++ src/app/Components/header/header.component.ts
@@ -0,0 +1,87 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+import Dexie from 'dexie';
+
+
+interface Values {
+  id?: number;
+  type?: string;
+  value?: number;
+}
+
+//
+// Declare Database
+//
+class ValuesDatabase extends Dexie {
+  values: Dexie.Table<Values, number>;
+
+  constructor() {
+    super('rhf76052_values');
+    this.version(1).stores({
+      values: '++id,type,value'
+    });
+  }
+}
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+
+export class HeaderComponent implements OnInit {
+
+  @Input() title: string;
+
+  clickMessage = '';
+
+  onClickMe() {
+
+    const db = new ValuesDatabase();
+
+    db.transaction('rw', db.values, async() => {
+
+      // // Make sure we have something in DB:
+      // if ((await db.values.where('name').equals('Josephine').count()) === 0) {
+      //   let id = await db.friends.add({name: "Josephine", age: 21});
+      //   alert (`Addded friend with id ${id}`);
+      // }
+
+      // Query:
+      let storedValues = await db.values.toArray();
+      this.clickMessage = JSON.stringify(storedValues);
+
+      // Show result:
+      // alert ("My young friends: " + JSON.stringify(youngFriends));
+
+    }).catch(e => {
+      console.log(e.stack || e);
+    });
+
+    // const db = new Dexie('rhf76052_values');
+    // db.version(1).stores({
+    //   values: '++id,type,value'
+    // });
+    //
+    // db.values.add({type: "Potência", value: receivedValue}).then(function() {
+    //   return db.values.toArray();
+    // }).then(function (storedValues) {
+    //   this.clickMessage = JSON.stringify(storedValues);
+    //     //console.log('Valores' + JSON.stringify(storedValues));
+    // }).catch(function (e) {
+    //   console.log('Error: ' + (e.stack || e));
+    // });
+    // '' 'You are my hero!';
+  }
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
+
+
+
+

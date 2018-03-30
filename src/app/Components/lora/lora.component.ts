b8bbb68882a2144a1cad278aa69999b6c65868aa
josiasMO <jotamo.jmo10@gmail.com>
2018-03-30 18:24:09 -0300

    Added support to download CSV file

--- src/app/Components/lora/lora.component.ts
+++ src/app/Components/lora/lora.component.ts
@@ -3,12 +3,13 @@ import {MatTableDataSource, MatSort} from '@angular/material';
 
 import Dexie from 'dexie';
 
-
+declare const Buffer;
 
 export interface Values {
   id?: number;
   counter?: number;
   payload_raw?: string;
+  port?: number;
   airtime?: number;
   coding_rate?: string;
   data_rate?: string;
@@ -29,7 +30,7 @@ class ValuesDatabase extends Dexie {
   constructor() {
     super('bufsm');
     this.version(1).stores({
-      values: '++id, counter, payload_raw, airtime, coding_rate, data_rate, frequency, timestamp,' +
+      values: '++id, counter, payload_raw, port airtime, coding_rate, data_rate, frequency, timestamp,' +
       'gtw_id, gtw_channel, gtw_rssi, gtw_snr'
     });
   }
@@ -46,6 +47,7 @@ export class LoraComponent implements OnInit {
   loadTable: boolean = false;
   dataSource;
 
+  DB_VALUES: Values[] = [];
   @ViewChild(MatSort) sort: MatSort;
 
   constructor() {
@@ -65,11 +67,12 @@ export class LoraComponent implements OnInit {
       // console.log(JSON.stringify(storedValues));
 
 
-      const DB_VALUES: Values[] = [];
+
       for (let i = 0; i < storedValues.length; i++){
-          DB_VALUES[i] = {id: storedValues[i].id,
+          this.DB_VALUES[i] = {id: storedValues[i].id,
             counter: storedValues[i].counter,
             payload_raw: storedValues[i].payload_raw,
+            port: storedValues[i].port,
             airtime: storedValues[i].airtime,
             coding_rate: storedValues[i].coding_rate,
             data_rate: storedValues[i].data_rate,
@@ -80,10 +83,12 @@ export class LoraComponent implements OnInit {
             gtw_rssi: storedValues[i].gtw_rssi,
             gtw_snr: storedValues[i].gtw_snr};
       }
-      console.log(DB_VALUES);
-      console.log(ELEMENT_DATA);
+      console.log(this.DB_VALUES);
 
-      this.dataSource = new MatTableDataSource(DB_VALUES);
+      let string_msg = Buffer.from(this.DB_VALUES[0].payload_raw, 'base64').toString();
+      console.log('Mensagem: -29.' + string_msg.slice(0, 5) + ',  -53.' + string_msg.slice(5));
+
+      this.dataSource = new MatTableDataSource(this.DB_VALUES);
       this.loadTable = true;
 
       this.dataSource.sort = this.sort;
@@ -96,15 +101,8 @@ export class LoraComponent implements OnInit {
     });
 
   }
-  displayedColumns = ['id', 'counter', 'payload_raw', 'airtime', 'coding_rate', 'data_rate',
+  displayedColumns = ['id', 'counter', 'payload_raw', 'port', 'airtime', 'coding_rate', 'data_rate',
     'frequency', 'timestamp', 'gtw_id', 'gtw_channel', 'gtw_rssi', 'gtw_snr'];
-
-  // displayedColumns = ['position', 'name', 'weight', 'symbol'];
-
-  // dataSource = new MatTableDataSource(ELEMENT_DATA);
-  // dataSource:  Object[];
-  // dbValues: Object[];
-
 
 
 
@@ -117,39 +115,37 @@ export class LoraComponent implements OnInit {
   }
 
 
+  convertToCSV(objArray) {
+  const array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
+  let str = 'id, counter, payload_raw, port, airtime, coding_rate, data_rate, frequency, ' +
+    'timestamp, gtw_id, gtw_channel, gtw_rssi, gtw_snr' + '\r\n';
 
+  for (let i = 0; i < array.length; i++) {
+    let line = '';
+    for (let index in array[i]) {
+      if (line != '') line += ','
+
+      line += array[i][index];
+    }
+
+    str += line + '\r\n';
+  }
+
+  return str;
+  }
+
+  downloadCSV(){
+
+    const jsonObject = JSON.stringify(this.DB_VALUES);
+
+    const csv = this.convertToCSV(jsonObject);
+    const blob = new Blob([csv], {type: 'text/csv;charset=utf-8;' });
+    const csvURL = window.URL.createObjectURL(blob);
+    const tempLink = document.createElement('a');
+    tempLink.href = csvURL;
+    tempLink.setAttribute('download', 'ActiveEvent_data.csv');
+    tempLink.click();
+  }
 
 }
-
-
-export interface Element {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: Element[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-  {position: 11, name: 'Sodium', weight: 22.9897, symbol: 'Na'},
-  {position: 12, name: 'Magnesium', weight: 24.305, symbol: 'Mg'},
-  {position: 13, name: 'Aluminum', weight: 26.9815, symbol: 'Al'},
-  {position: 14, name: 'Silicon', weight: 28.0855, symbol: 'Si'},
-  {position: 15, name: 'Phosphorus', weight: 30.9738, symbol: 'P'},
-  {position: 16, name: 'Sulfur', weight: 32.065, symbol: 'S'},
-  {position: 17, name: 'Chlorine', weight: 35.453, symbol: 'Cl'},
-  {position: 18, name: 'Argon', weight: 39.948, symbol: 'Ar'},
-  {position: 19, name: 'Potassium', weight: 39.0983, symbol: 'K'},
-  {position: 20, name: 'Calcium', weight: 40.078, symbol: 'Ca'},
-];
-
 
